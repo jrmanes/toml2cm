d3dd587c6bca3e9c463e06f131c42fe2d4a8dbb9
Jose Ramon Mañes Blasco <jr.mb47@gmail.com>
2023-02-03 22:35:40 +0100

    Refactor to some functions

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	// spacesToText yaml format spaces in a ConfigMap
+	spacesToText = "    "
+)
+
+// Run start the service here
+func Run() {
+	// filePath path to the file that we'll scan
+	file := flag.String("file", "./test.toml", "Toml file to convert to Kubernetes ConfigMap")
+
+	flag.Parse()
+
+	// Open file
+	f, err := os.Open(*file)
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	// scan the file line by line
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	// close the file when the function will finish
+	defer f.Close()
+
+	fileNameCleaned := cleanUpFileName(*file)
+	fmt.Println("File Name: ", fileNameCleaned)
+
+	// Check line by line
+	for _, eachline := range txtlines {
+		// add the spaces to the beginning of the line - yaml format
+		eachline = spacesToText + eachline
+
+		if strings.Contains(eachline, "=") {
+			eachline = formatLine(eachline)
+		}
+
+		fmt.Println(eachline)
+	}
+}
+
+func formatLine(line string) string {
+	l := line[:strings.IndexByte(line, '=')]
+
+	l = strings.ReplaceAll(l, " ", "")
+	line = strings.ReplaceAll(line, "= ", " = {{ .Values.configMaps.FILENAME."+l+" | "+"default "+"\"")
+	line = line + "\"" + " | quote }}"
+
+	// example line
+	// broadcast-mode = {{ .Values.configMaps.client_toml.broadcast-mode | default "sync" | quote }}
+	return line
+}
+
+// cleanUpFileName clean format in the file name
+func cleanUpFileName(f string) string {
+	// change . to _ in the filename
+	f = strings.ReplaceAll(f, ".", "_")
+	// change - to _ in the filename
+	f = strings.ReplaceAll(f, "-", "_")
+
+	fmt.Println("--------------------------")
+	fmt.Println(f)
+	fmt.Println("--------------------------")
+
+	return f
+}
+
+// main everything starts here!
+func main() {
+	// Run -> entrypoint
+	Run()
+}
